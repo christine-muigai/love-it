f32cf951f776512fa512d1fe3ad41ec9ddd4a478
christine-muigai <git@github.com>
2025-04-14 19:20:54 +0300

    Auto commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function displayWelcomeMessage() {
+    console.log("Welcome to the Lovely Website! We hope you enjoy your visit.");
+}
+displayWelcomeMessage();
+
+console.log("Current Date and Time:", new Date().toLocaleString());
